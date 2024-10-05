9397118c87666c3a638595ea2479c3f4319cf9de
Surya Narayanan <suryabhoopendranath@gmail.com>
2024-10-05 09:57:33 +0530

    update index

--- app.js
+++ app.js
@@ -18,19 +18,19 @@ const jobRouter = require("./routes/jobs");
 // const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.set("trust proxy", 1);
-app.use(
-  rateLimiter({
-    windows: 15 * 60 * 1000,
-    max: 100,
-  })
-);
+// app.set("trust proxy", 1);
+// app.use(
+//   rateLimiter({
+//     windows: 15 * 60 * 1000,
+//     max: 100,
+//   })
+// );
 
-app.use(express.json());
-app.use(helmet());
-app.use(cors());
-app.use(xss());
-app.use(rateLimiter);
+// app.use(express.json());
+// app.use(helmet());
+// app.use(cors());
+// app.use(xss());
+// app.use(rateLimiter);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
